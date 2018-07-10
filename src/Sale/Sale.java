d5edea5778d4e3bc70f4c5f42d3d164224ad6ec5
ilamkeo <ilamkeo@hotmail.com>
2018-07-10 17:38:17 +0700

    add product for sale

--- src/Sale/Sale.java
+++ src/Sale/Sale.java
@@ -6,22 +6,30 @@
 package Sale;
 
 import Stock.Product;
+import java.util.ArrayList;
 
 /**
  *
  * @author Keo
  */
 public class Sale {
-
+    String name;
+    int id,price;
+ ArrayList<Product> pro = new ArrayList<Product>();
     public void addProduct(Product p1) {
-        
+        pro.add(p1);
     }
 
     public double getTotalPrice() {
-        return 0;
+        int sum=0;
+        for(Product p: pro){
+            sum += p.price();
+        }
+        return sum;
     }
 
     public Product findProduct(String id) {
+        
         return null;        
     }
 }
