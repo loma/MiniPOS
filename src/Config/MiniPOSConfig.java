6d55d3dd776b0496a22de43f37e29396a43705fe
loma <57.loma@gmail.com>
2018-07-17 16:39:27 +0700

    add config back

---
+++ src/Config/MiniPOSConfig.java
@@ -0,0 +1,17 @@
+package Config;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author loma
+ */
+public class MiniPOSConfig {
+    public static final String DB_NAME = "MiniPOS"; 
+    public static final String DB_USERNAME = "mini"; 
+    public static final String DB_PASSWORD = "mini"; 
+}
