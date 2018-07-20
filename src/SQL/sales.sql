5fdadbea17be0d28afd36c3fc432855019bb0177
loma <57.loma@gmail.com>
2018-07-20 13:29:24 +0700

    add sales and sale_details sql

---
+++ src/SQL/sales.sql
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  loma
+ * Created: Jul 20, 2018
+ */
+CREATE TABLE `sales` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `total` double NOT NULL,
+ `discount` double NOT NULL,
+ `paid` double NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+
