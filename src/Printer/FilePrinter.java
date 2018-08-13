bc0fea735332d60beaa9b99c79c83d1a3f7d7667
loma <57.loma@gmail.com>
2018-08-13 17:14:31 +0700

    add FilePrinter

---
+++ src/Printer/FilePrinter.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Printer;
+
+import Sale.Order;
+
+/**
+ *
+ * @author loma
+ */
+public class FilePrinter implements IPrinter {
+
+    @Override
+    public void print(Order order) {
+        // print to file by id
+    }
+    
+}
